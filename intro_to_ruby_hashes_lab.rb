21cb4042e66247ff4f417bfb2814cce89e3f68c8
Eric Manning <emanningmusic@yahoo.com>
2019-09-30 21:45:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  update_hash[hash] + 1 
+  update_hash[key] + 1 
 end
 
 # given a hash an a key as parameters, return an updated hash
